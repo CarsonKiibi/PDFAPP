f2b0574634ea83faf0d4650b1288282b19f6ac9f
Carson K <ckyba@students.cs.ubc.ca>
2024-07-01 23:21:15 -0700

    -changes to HandleDocumentSpacing function

--- backend/process/commands/helpers.go
+++ backend/process/commands/helpers.go
@@ -1,21 +1,29 @@
 package commands
 
+import (
+	"strconv"
+)
+
 // ignores the command following the symbol, returns token as just text
 func HandleIgnoreCommand(command string) Token {
 	return Token{}
 }
 
-// todo
+// fix return tokens, test!
 func HandleDocumentSpacing(input string) []Token {
 	dir, size, err := SplitTextModifier(input)
 	if err != nil {
 		return []Token{{Attributes: TokenAttributes{Error: err}}}
 	}
 
-	if len(dir) != 1 {
-		return []Token{{Literal: size, Attributes: TokenAttributes{}}}
+	s, err := strconv.Atoi(size)
+	if err != nil {
+		return []Token{{Type: TokenIllegalCharacter}}
 	}
-	return []Token{{Literal: size, Attributes: TokenAttributes{}}}
+	if len(dir) != 1 {
+		return []Token{{Literal: dir, Attributes: TokenAttributes{Size: s}}}
+	}
+	return []Token{{Literal: dir, Attributes: TokenAttributes{}}}
 }
 
 func HandleBulletStart(command string) Token {
@@ -60,7 +68,7 @@ func HandleOpenSquareBrace(inBraces bool, word []rune, tokens []Token, i int) ([
 
 func HandleClosedSquareBrace(inBraces bool, word []rune, tokens []Token, i int) ([]Token, []rune, bool) {
 	if inBraces {
-		tokens = append(tokens, HandleTextModifier(string(word[1:]))...)
+		tokens = append(tokens, HandleDocumentSpacing(string(word[1:]))...)
 		word = []rune{}
 		inBraces = false
 	} else {
