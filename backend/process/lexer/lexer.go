43c543b8c4a6df85136084680e59f7cdf4e6a149
Carson K <ckyba@students.cs.ubc.ca>
2024-07-17 23:33:54 -0700

    -currently can pick out words and some symbols -need to make text mod/others work since it just spits out words and brackets right now -need to figure out design of nested stuff and all that

--- backend/process/lexer/lexer.go
+++ backend/process/lexer/lexer.go
@@ -5,9 +5,28 @@ import (
 	"fmt"
 	"io"
 	"strings"
-
-	"github.com/carsonkiibi/pdfapp/backend/process/commands"
+	"unicode"
 )
+
+type Token int
+
+const (
+	EOF = iota 
+	ILLEGALNEST
+	ILLEGAL 
+	SPACING 
+	TEXTMOD 
+	TEXT 
+)
+
+var tokens = []string{
+	EOF: 	"EOF",
+	ILLEGALNEST: "ILLEGALNEST",
+	ILLEGAL: "ILLEGAL",
+	SPACING: "SPACING",
+	TEXTMOD: "TEXTMOD",
+	TEXT: "TEXT",
+}
 
 type Position struct {
 	line   int
@@ -19,6 +38,10 @@ type Lexer struct {
 	reader *bufio.Reader
 }
 
+func (t Token) String() string {
+	return tokens[t]
+}
+
 func NewLexer(reader io.Reader) *Lexer {
 	return &Lexer{
 		pos:    Position{line: 1, column: 0},
@@ -26,57 +49,86 @@ func NewLexer(reader io.Reader) *Lexer {
 	}
 }
 
-func (l *Lexer) Lex() (Position, commands.Token, string) {
+func (l *Lexer) Lex() (Position, Token, string) {
 	for {
+		l.pos.column++
 		r, _, err := l.reader.ReadRune()
 		if err != nil {
 			if err == io.EOF {
-				return l.pos, commands.Token{Literal: "end"}, ""
+				return l.pos, EOF, ""
 			}
 			panic(err)
 		}
 
-		if r == '{' {
-			content, err := l.lexTextMod()
-			if err != nil {
-				panic(err)
+		switch r {
+		case '\\':
+			// found it!
+		case '\n':
+			l.resetPosition()
+		case '{':
+			return l.pos, TEXT, "{"
+		case '}':
+			return l.pos, TEXT, "}"
+		case '[':
+			return l.pos, TEXT, "["
+		case ']':
+			return l.pos, TEXT, "]"
+		default:
+			if unicode.IsSpace(r) {
+				continue
+			} else if unicode.IsLetter(r) || unicode.IsNumber(r) {
+				startPos := l.pos
+				l.backup()
+				lit := l.lexTextMod()
+				return startPos, TEXTMOD, lit 
 			}
-			return l.pos, commands.Token{Literal: string(r)}, content
 		}
 	}
+	
+}
+
+func (l *Lexer) resetPosition() {
+	l.pos.line++
+	l.pos.column = 0
 }
 
 // need to test this
-func (l *Lexer) lexTextMod() (string, error) {
+func (l *Lexer) lexTextMod() string {
 	var sb strings.Builder
-
 	for {
 		r, _, err := l.reader.ReadRune()
 		if err != nil {
-			return "", fmt.Errorf("unexpected EOF")
+			if err == io.EOF {
+				return sb.String()
+			}
 		}
-
-		switch string(r) {
-		case "{", "}":
-			return string(r), nil
+		l.pos.column++ 
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			sb.WriteRune(r)
+		} else {
+			return sb.String()
 		}
-
-		sb.WriteRune(r)
 	}
+}
 
-	return sb.String(), nil
+func (l *Lexer) backup() {
+	if err := l.reader.UnreadRune(); err != nil {
+		panic(err)
+	}
+	
+	l.pos.column--
 }
 
 func main() {
-	input := "hello {hello} hello"
+	input := "tok1 { tok2 [ \n tok3"
 	reader := strings.NewReader(input)
 	lexer := NewLexer(reader)
 	for {
 		pos, tok, lit := lexer.Lex()
-		if tok.Literal == "end" {
+		if tok == EOF {
 			break
 		}
 
-		fmt.Printf("%d:%d | \t%s | \t%s | \n", pos.line, pos.column, tok.Literal, lit)
+		fmt.Printf("%d:%d | %s | %s | \n", pos.line, pos.column, tok, lit)
 	}
 }
